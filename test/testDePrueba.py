00919ca967b37eba2a0f363859cc2ee13f59bdf4
Andres Martinez <af.martinezc1@uniandes.udu.co>
2023-03-08 21:06:36 -0500

    esto es un MERGE_MAIN

--- test/testDePrueba.py
+++ test/testDePrueba.py
@@ -11,7 +11,7 @@ class PersonaTestCase(unittest.TestCase):
       self.persona3 = Persona(nombre='Alejandra', edad=25)
       self.persona4 = Persona(nombre='Dianaasd', edad=25)
       self.grupo = [self.persona1, self.persona2, self.persona3]
-      self.grupo = [self.persona1, self.persona1, self.persona1]
+      self.grupo = [self.persona1, self.persona2, self.persona3]
 
    def test_constructor(self):
       self.assertEqual(self.persona1.dar_nombre(), 'Alejandra')
