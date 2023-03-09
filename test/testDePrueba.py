886f5be4ed083eacc1645b73519f18f016aadef9
Andres Martinez <af.martinezc1@uniandes.udu.co>
2023-03-08 20:37:26 -0500

    esto es un MERGE_MAIN

--- test/testDePrueba.py
+++ test/testDePrueba.py
@@ -9,7 +9,7 @@ class PersonaTestCase(unittest.TestCase):
       self.persona1 = Persona(nombre='Alejandra', edad=25)
       self.persona2 = Persona(nombre='Diego', edad=22)
       self.persona3 = Persona(nombre='Alejandra', edad=25)
-      self.persona4 = Persona(nombre='Diana', edad=25)
+      self.persona4 = Persona(nombre='Dianaasd', edad=25)
       self.grupo = [self.persona1, self.persona2, self.persona3]
 
    def test_constructor(self):
